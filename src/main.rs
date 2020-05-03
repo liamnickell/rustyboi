d967bba531a722c0565b013a193e440877903bad
celebi1023 <37511861+celebi1023@users.noreply.github.com>
2020-05-03 17:36:19 -0500

    added rom.rs

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,7 @@ extern crate minifb;
 
 use minifb::{Key, Window, WindowOptions};
 
-use std::error::Error;
-use std::fs::File;
-use std::io::prelude::*;
-use std::path::Path;
+mod rom;
 
 const WIDTH: usize = 160;
 const HEIGHT: usize = 144;
@@ -13,17 +10,7 @@ const HEIGHT: usize = 144;
 fn main() {
     //open rom
     let romName = "../Roms/tetris.gb";
-    let path = Path::new(romName);
-    let display = path.display();
-    let mut file = match File::open(&path) {
-        Err(why) => panic!("couldn't open {}: {}", display,why.description()),
-        Ok(file) => file,
-    };
-    let mut rom = Vec::new();
-    file.read_to_end(&mut rom);
-    println!("{:?}", rom);
-    //I'm not entirely sure if what's opened is correct^
-    //but we should probably upload this into mem afterwards
+    rom::openRom(romName);
 
 
    //create window
---
+++ src/rom.rs
@@ -0,0 +1,21 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
+
+pub fn openRom(name: &str){
+    //let romName = *name;
+    let path = Path::new(name);
+    let display = path.display();
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display,why.description()),
+        Ok(file) => file,
+    };
+
+    let mut romData = Vec::new();
+    file.read_to_end(&mut romData);
+    println!("{:?}", romData);
+    //I'm not entirely sure if what's opened is correct^
+    //but we should probably upload this into mem afterwards
+}
