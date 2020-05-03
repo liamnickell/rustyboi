cb44891259f240483f4e5d64a8757f88837a2f74
KusakabeMirai <30453425+KusakabeMirai@users.noreply.github.com>
2020-05-02 21:20:38 -0500

    created register.rs

---
+++ src/register.rs
@@ -0,0 +1,19 @@
+pub struct Registers{
+    pub a: u8,
+    pub b: u8,
+    pub c: u8,
+    pub d: u8,
+    pub e: u8,
+    pub f: u8,
+    pub h: u8,
+    pub l: u8,
+    pub pc: u16,
+    pub sp: u16,
+}
+
+pub struct Flag{
+    C : bool, // carry
+    H : bool, // half carry
+    Z : bool, // is zero
+    S : bool, // is substract
+}
