341b7ebb739af1b7f1437ddacd483574c1747eb2
celebi1023 <37511861+celebi1023@users.noreply.github.com>
2020-05-03 01:44:17 -0500

    display structure main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+extern crate minifb;
+
+use minifb::{Key, Window, WindowOptions};
+
+const WIDTH: usize = 640;
+const HEIGHT: usize = 360;
+
 fn main() {
-    println!("Hello, world!");
+    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
+
+    let mut window = Window::new(
+        "Rusty Boi ;)",
+        WIDTH,
+        HEIGHT,
+        WindowOptions::default(),
+    )
+    .unwrap_or_else(|e| {
+        panic!("{}", e);
+    });
+
+    // Limit to max ~60 fps update rate
+    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
+
+    while window.is_open() {
+        for i in buffer.iter_mut() {
+            *i = 0xff0000; 
+        }
+
+        
+        window.update_with_buffer(&buffer, WIDTH, HEIGHT);
+    }
 }
