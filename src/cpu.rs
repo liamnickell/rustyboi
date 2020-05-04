9650affaa9927da7999d549fc686f9253f2079cf
Liam Nickell <liam.nickell25@gmail.com>
2020-05-04 07:58:17 -0500

    added more to cpu structure

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,33 +1,87 @@
 pub struct CPU {
-    // regs, fetch data, cycles, etc.
+    regs: Regs,
+    mmu: MMU,
+    opcode: u8,
+    cycles: u8,
+    clock: Clock,
+    halted: bool,
 }
 
 impl CPU {
     pub fn init() -> CPU {
         CPU {
-            // initialize stuff
+            regs: Regs::init(),
+            clock: Clock::init(),
+            mmu: MMU::init(clock),
+            halted: false,
         }
     }
 
-    pub fn next() {
-        fetch();
-        decode();
-        add_cycles();
+    pub fn fetch_byte(&mut self) -> u8 {
+        let opcode = self.mmu.read_byte(pc);        // fetch op
+        self.regs.set_pc(regs.pc() + 1);            // pc++
+        opcode
     }
 
-    pub fn add_cycles() {
-        // add recorded cycles
+    pub fn fetch_word(&mut self) -> u16 {
+        let data = self.mmu.read_word(pc);          // fetch data
+        self.regs.set_pc(regs.pc() + 2);            // pc += 2
+        data
     }
 
-    pub fn fetch() {
-        // opcode = read(pc)
-        // pc++
+    pub fn run(&mut self) {
+        let opcode = self.fetch_byte();
+        match opcode {
+            0x00 => self.nop(),
+            0x01 => self.ld(),
+            0x02 => ,
+            0x03 => ,
+            0x04 => ,
+            0x05 => ,
+            0x06 => ,
+            0x07 => ,
+            0x08 => ,
+            0x09 => ,
+            0x0a => ,
+            0x0b => ,
+            0x0c => ,
+            0x0d => ,
+            0x0e => ,
+            0x0f => ,
+            0x10 => self.stop(),
+            // other instructions
+        }
     }
 
-    pub fn decode() {
-        // Instruction ins  = match opcode { opcodes => Instruction(string, src, dest, ins, cycles) }
-    }
-
+    
     // general execute instruction functions with parameters
 
+    fn nop() {
+        4
+    }
+
+    // move 16 bit value from src to dest
+    fn ld(src: fn() -> u16, dest: fn(u16)) {
+
+    }
+
+    // move 8 bit value from src to dest
+    fn ld(src: fn() -> u8, dest: fn(u8)) {
+
+    }
+
+    fn stop() {
+        if self.fetch_byte() == 0x00 {
+            self.halt = true;
+            // other stuff ?
+        }
+
+        4
+    }
+
+    fn halt() {
+        self.halt = true;
+        // other stuff ?
+        4
+    }
 }
